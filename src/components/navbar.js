eacefee6874c2f8ba0e56d9144b7d087ee6f4ce3
RAGAdox <rishitech882@gmail.com>
2021-05-13 16:51:47 +0530

    Shaking in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => {
     setNavVisible(!navVisible);
   };
   return (
-    <div className="bg-gray-800 text-gray-300 flex flex-col sm:flex-row justify-between text-2xl py-1 sticky top-0 z-50">
-      <div className="flex flex-row justify-between px-6 shadow-sm">
+    <div className="bg-gray-800 text-gray-300 flex flex-col sm:flex-row justify-between text-2xl  sticky top-0 z-50">
+      <div className="flex flex-row justify-between px-6 shadow-sm ">
         <a href="/" className="flex flex-row items-center h-full ">
-          <img src={logo} className=""></img>
-          Rishi
+          <img src={logo} className="self-center"></img>
+          <span className="self-center">Rishi</span>
         </a>
         <span className="flex self-center sm:hidden" onClick={toggleMenu}>
           <i className={`fas fa-bars ${navVisible ? "hidden" : "inline"}`}></i>
@@ -19,13 +19,13 @@ const Navbar = () => {
         </span>
       </div>
       <div
-        className={`sm:flex sm:flex-row ${
+        className={`sm:flex sm:flex-row  sm:p-0 sm:mt-0 ${
           !navVisible ? "hidden" : "flex flex-col px-5 mt-1 py-2"
         }`}
       >
         <a
           href="/"
-          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 ${
+          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 focus:bg-gray-900 active:bg-green-700 sm:mx-0 ${
             !navVisible ? "mx-1" : ""
           }`}
         >
@@ -33,7 +33,7 @@ const Navbar = () => {
         </a>
         <a
           href="/"
-          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 ${
+          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 focus:bg-gray-900 active:bg-green-700 sm:mx-0 ${
             !navVisible ? "mx-1" : ""
           }`}
         >
@@ -41,7 +41,7 @@ const Navbar = () => {
         </a>
         <a
           href="/"
-          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 ${
+          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 focus:bg-gray-900 active:bg-green-700 sm:mx-0 ${
             !navVisible ? "mx-1" : ""
           }`}
         >
@@ -49,7 +49,7 @@ const Navbar = () => {
         </a>
         <a
           href="/"
-          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 ${
+          className={`flex px-6  h-full content-center items-center hover:bg-gray-900 focus:bg-gray-900 active:bg-green-700 sm:mx-0 ${
             !navVisible ? "mx-1" : ""
           }`}
         >
